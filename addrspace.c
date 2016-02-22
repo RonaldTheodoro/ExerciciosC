8a001670492e0e7feecd8ee7f3b3c31af0406a91
Ronald Theodoro Nascimento Silva <ronaldtheodoro@gmail.com>
2016-02-22 18:02:42 -0300

    Create addrspace.c

---
+++ addrspace.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int varglobal;
+
+
+void func1 (void) {
+    int var1, var2;
+}
+
+
+void func2 (void) {
+    char str1, str2;
+}
+
+
+int main () {
+    float calc;
+    
+    return 0;
+}
