dd8fd5c523ff990d4b8cd9bd4e556cf12f73f75d
Ronald Theodoro Nascimento Silva <ronaldtheodoro@gmail.com>
2016-02-24 18:05:49 -0300

    Create debug.c

---
+++ cap03/debug.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+/* Função com lista  de argumentos variaveis */
+static void debug (const char *fmt, ...) {
+
+}
+
+
+int main () {
+
+    return 0;
+}
